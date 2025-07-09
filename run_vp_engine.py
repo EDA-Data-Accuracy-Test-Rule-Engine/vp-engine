0c9d7cdc4be32c688b0f4dec978e02db9ea36bc4
namf2001 <ndkhanh.dhtd15a6hn@sv.uneti.edu.vn>
2025-07-09 10:34:32 +0700

    chore: Remove VP Engine CLI launcher script

--- run_vp_engine.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-"""
-VP Engine CLI Launcher
-Easy way to start the interactive data validation platform
-"""
-
-if __name__ == "__main__":
-    from src.cli.main import cli
-    cli()
